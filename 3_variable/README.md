776f5ed28d3050bf3cd0564611a7386e08e8fae7
Ayan <ayan2572006@gmail.com>
2024-11-26 01:43:55 +0530

    variables

---
+++ 3_variable/README.md
@@ -0,0 +1,1 @@
+Variable Declearation On Golang
---
+++ 3_variable/var.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Ayan Hasnain"
+
+	//type infer
+	// var name = "ayan hasnain"
+	// fmt.Println(name)
+
+	// var isAdult = true
+	// fmt.Println(isAdult)
+
+	// var age = 30
+
+	// fmt.Println(age)
+
+	//short-hand way
+	// name := "Ayan Hansain"
+	// fmt.Println(name)
+
+	//when you assigned value after
+
+	// var name string
+	// name = "ayan"
+	// println(name)
+
+	var price = 50.5
+	fmt.Println(price)
+
+}
