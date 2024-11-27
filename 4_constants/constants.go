1752afc6b8e1c77b46c0dacf3a9c2db699c6e9a1
Ayan <ayan2572006@gmail.com>
2024-11-28 02:40:07 +0530

    constants

---
+++ 4_constants/constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const name string = "Ayan Hasnain"
+	//Not Change
+	//name="golang"
+	const (
+		name = "Ayan Hasnain"
+		age  = 30
+	)
+
+	fmt.Println(name, age)
+}
